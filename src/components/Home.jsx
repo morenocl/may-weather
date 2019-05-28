c346732be94ffdeb88980483bdbd4d8265c3cfa5
Gonzalo Gigena <gongigena@hotmail.com>
2019-05-28 11:08:06 -0300

    Gradient added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ class Home extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            weather: {name: "prueba"},
+            weather: [],
             forecast: [],
             city: 'cordoba',
             type: 0,
