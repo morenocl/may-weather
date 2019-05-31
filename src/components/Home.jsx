f936b0fe0813d6e5b623a84cb4882692b201bcb7
gongigena <gongigena@hotmail.com>
2019-05-31 12:00:51 -0300

    New logo, eslint stylecode

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
 import Card from '@material-ui/core/Card';
 import Fade from '@material-ui/core/Fade';
 import Box from '@material-ui/core/Box';
-import Search from "./Search";
-import Slide from "./Slide";
-import Api from "../api";
+import CardMedia from '@material-ui/core/CardMedia';
+import Search from './Search';
+import Slide from './Slide';
+import Api from '../api';
 import '../css/home.css';
+// import logo from '../img/logo.png'; // Tell Webpack this JS file uses this image
 
-class Home extends React.Component{
+class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -16,22 +18,20 @@ class Home extends React.Component{
       city: '',
       loading: false,
       checked: false,
-      unMount: false,     // Renderizar informacion
-    }
-    this.api = new Api();
-    this.updateCity = this.updateCity.bind(this)
-    this.updateLoading = this.updateLoading.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-  };
+      unMount: false, // Renderizar informacion
+    };
+    this.updateCity = this.updateCity.bind(this);
+    this.updateLoading = this.updateLoading.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
   updateLoading() {
     this.setState({
-        loading: !this.state.loading,
+      loading: !this.state.loading,
     });
   }
 
   updateCity(newCity) {
-    // event.preventDefault();
     this.setState({
       city: newCity,
     });
@@ -40,22 +40,25 @@ class Home extends React.Component{
 
   async updateWeather(newCity) {
     this.updateLoading();
-    const responseWeather = await this.api.getWeather(newCity);
-    const responseForecast = await this.api.getForecast(newCity);
+    const api = new Api();
+    const responseWeather = await api.getWeather(newCity);
+    const responseForecast = await api.getForecast(newCity);
     const jsonWeather = await responseWeather.json();
     const jsonForecast = await responseForecast.json();
-    let check = true, unmount = true;
-    
-    if (jsonWeather.cod == "400" || jsonWeather.cod == "404") { // Cambiar
+    let check = true; let
+      unmount = true;
+
+    if (jsonWeather.cod == '400' || jsonWeather.cod == '404') { // Cambiar
       check = false;
       unmount = false;
     }
+
     this.setState({ checked: check }, () => {
       setTimeout(() => {
         this.setState({ unMount: unmount });
       }, 200);
     });
-    
+
     this.setState({
       weather: jsonWeather,
       forecast: jsonForecast,
@@ -69,25 +72,38 @@ class Home extends React.Component{
       checked: !this.state.checked,
     });
   }
+
   render() {
     const { checked } = this.state;
-    return(
+    return (
       <div>
-      <Box className="Search">
-        <Search updateLoading={this.updateLoading} 
-                updateCity={this.updateCity}
-                loading={this.state.loading}
-                api={this.api}></Search>
-      </Box>
-      { this.state.unMount &&
+        <Box className="logo">
+          <img src="https://static1.squarespace.com/static/5931d6a5b8a79b4f41d4eba6/t/593998c829687fc903474f9d/1551899707003/" alt="queonda"/>
+          {/* <CardMedia
+            image={logo}
+            title="Contemplative Reptile"
+            // className={classes.media}
+          /> */}
+        </Box>
+        <Box className="Search">
+          <Search
+            updateLoading={this.updateLoading}
+            updateCity={this.updateCity}
+            loading={this.state.loading}
+          />
+        </Box>
+        { this.state.unMount
+        && (
         <Fade in={checked}>
           <Card className="Home">
-            <Slide data={this.state}></Slide>
+            <Slide data={this.state} />
           </Card>
         </Fade>
+        )
       }
       </div>
-    );    
+    );
   }
 }
+
 export default Home;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,47 +1,30 @@
-import React from "react";
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import AsyncSelect from 'react-select/async';
+import Api from '../api';
 import '../css/Search.css';
 
-
-import AsyncSelect from 'react-select/async';
-
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
-
-const api_key = "092666ce8b5be26d8e87904c227fb522";
-const url = "https://api.openweathermap.org/data/2.5";
-const lang = "es";
-
 function promiseOptions(inputValue) {
-  return fetch(url+"/find?q="+inputValue+"&APIKEY=" + api_key)
-  .then((response) => {
-    return response.json();
-  }).then((json) => {
-    var options = [];
-    json.list.map(elem => 
-      options.push({
-        value: elem.name +", "+ elem.sys.country, 
-        label: elem.name +", "+ elem.sys.country})
-    );
+  return new Api().find(inputValue).then(response => response.json()).then((json) => {
+    const options = [];
+    json.list.map(elem => options.push({
+      value: `${elem.name}, ${elem.sys.country}`,
+      label: `${elem.name}, ${elem.sys.country}`,
+    }));
     return options;
   });
-
 }
 
 function CustomSearch(props) {
   let icon;
   if (props.loading) {
-    icon =  <CircularProgress size={24}/>
+    icon = <CircularProgress size={24} />;
   } else {
-    icon = <SearchIcon />
+    icon = <SearchIcon />;
   }
   return (
     <Paper className="root">
@@ -50,8 +33,9 @@ function CustomSearch(props) {
         placeholder="Seleccionar ciudad"
         onChange={props.onChange}
         loadOptions={promiseOptions}
-        cacheOptions 
-        defaultOptions></AsyncSelect>
+        cacheOptions
+        defaultOptions
+      />
       <Divider />
       <IconButton className="iconButton" aria-label="Search" type="submit">
         {icon}
@@ -61,44 +45,41 @@ function CustomSearch(props) {
 }
 
 class Search extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      search: "",
+      search: '',
       loading: props.loading,
-    }
+    };
     this.handleOnChange = this.handleOnChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  };
+  }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({loading: nextProps.loading});
+    this.setState({ loading: nextProps.loading });
   }
 
-  handleSubmit(event){
-    console.log(event.target);
+  handleSubmit(event) {
     event.preventDefault();
-    this.props.updateCity(this.state.search); 
+    this.props.updateCity(this.state.search);
   }
-  
-  handleOnChange(newCity){
-    console.log(newCity.value);
-    this.setState({search:newCity.value});
+
+  handleOnChange(newCity) {
+    this.setState({ search: newCity.value });
   }
 
   render() {
-    return(
+    return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <CustomSearch 
+          <CustomSearch
             value={this.state.value}
             onChange={this.handleOnChange}
             loading={this.state.loading}
-          >
-          </CustomSearch>
+          />
         </form>
       </div>
-    );    
+    );
   }
 }
 
