4778164828184326d9d56e3cf71836f5476baef2
Christian L Moreno <christianluciano.m@gmail.com>
2019-06-06 18:24:50 -0300

    Add onClick

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -6,8 +6,9 @@ import { days, urlImg } from '../settings';
 
 function Forecast(props) {
   // today contiene el datallado cada 3 horas del dia.
-  const today = props.list.filter(
-    ele => (new Date(Number(ele.dt) * 1000)).getDay() === ((new Date()).getDay() + props.index) % 7,
+  const { list, index } = props;
+  const today = list.filter(
+    ele => (new Date(Number(ele.dt) * 1000)).getDay() === ((new Date()).getDay() + index) % 7,
   );
   // Calculo de temp max y min del dia.
   let max = -99;
@@ -22,9 +23,13 @@ function Forecast(props) {
   const fecha = `${date.getDate()}/${date.getMonth() + 1}`;
   const url = `${urlImg + today[3].weather[0].icon}.png`;
 
+  function handleClick() {
+    console.log('click hecho.');
+  }
+
   return (
     <Grid item sm>
-      <Paper className="gridItem">
+      <Paper className="gridItem" onClick={handleClick}>
         <p>
           {' '}
           { days[day] }
--- src/components/ForecastContainer.jsx
+++ src/components/ForecastContainer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Forecast from './Forecast';
 
 function ForecastContainer(prop) {
